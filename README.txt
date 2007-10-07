82f1978f3faedd8d78141cbacb215dc9d83d23fe
leidel <leidel@12edf5ea-513a-0410-8a8c-37067077e60f>
2007-10-07 12:43:56 +0000

    major refactoring, more options, right mimetype and correct http status codes for resulting robots.txt, crawl-delay support, Allow and Dissallow support, automatic Sitemap support
    git-svn-id: https://django-robots.googlecode.com/svn/trunk@6 12edf5ea-513a-0410-8a8c-37067077e60f

--- README.txt
+++ README.txt
@@ -1,94 +1,15 @@
-=======================================
-Robots exclusion application for Django
-=======================================
+=============
+Django Robots
+=============
 
 This is a basic Django application to manage robots.txt files following the
-`robots exclusion standard`_, complementing the Django `Sitemap contrib app`_.
+`robots exclusion protocol`_, complementing the Django_ `Sitemap contrib app`_.
 
-.. _robots exclusion standard: http://www.robotstxt.org/
+For installation instructions, see the file "INSTALL.txt" in this
+directory; for instructions on how to use this application, and on
+what it provides, see the file "overview.txt" in the "docs/"
+directory.
+
+.. _robots exclusion protocol: http://www.robotstxt.org/wc/exclusion.html
+.. _Django: http://www.djangoproject.com/
 .. _Sitemap contrib app: http://www.djangoproject.com/documentation/sitemaps/
-
-How to use it in your own django application
-============================================
-
-0. Get the source from the application site at::
-
-        http://code.google.com/p/django-robots/
-
-1. Follow the instructions in the INSTALL.txt file
-
-2. Edit the settings.py of your Django project::
-
-    # Add ``robots`` to the ``INSTALLED_APPS`` of your Django project
-
-    # Check if ``django.contrib.sites`` and ``django.contrib.admin`` are in
-      ``INSTALLED_APPS`` and add if necessary
-      
-      It should look something like this::
-      
-        INSTALLED_APPS = (
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.sites',
-            'django.contrib.admin',
-            'django.contrib.flatpages',
-            'robots',
-            'myproject.myapp',
-        )
-    
-    # Check if ``django.template.loaders.app_directories.load_template_source``
-      is in the ``TEMPLATE_LOADERS`` list.
-
-      It should look something like this::
-
-        TEMPLATE_LOADERS = (
-            'django.template.loaders.filesystem.load_template_source',
-            'django.template.loaders.app_directories.load_template_source',
-        )
-
-3. Add this line to your site's root URLConf::
-
-        (r'^robots.txt$', include('robots.urls')),
-
-4. Sync your database via shell (``manage.py syncdb`` in your project dir).
-
-5. Create ``Rule`` and ``Url`` objects in the admin interface.
-
-6. Go to /robots.txt under the URL of your Django site to see the results.
-
-Usage
-=====
-
-The application consists of two database models which are tied together with a
-m2m relationship::
-
-    1. ``Rule`` - contains a User Agent field and multiple URL patters to
-       define a abstract disallowance rule. Each rule will be rendered with
-       all of its related URLs in a simple and plain text file.
-
-       Please have a look at the `database of web robots`_ for a full list of
-       existing web robots user agent strings.
-
-    2. ``Url`` - defines a case-sensitive and exact URL pattern which is used
-       together with an user agent string to disallow the access for web robots.
-
-You can set ``SITEMAP_URL`` in your projects settings.py file to the URL of your
-sitemap.xml file, for example::
-
-    SITEMAP_URL = "http://www.djangoproject.com/sitemap.xml"
-
-This is added to the resulting robots.txt file as a "Sitemap:" statement - very
-useful especially in combination with the Django Sitemaps contrib app.
-
-If no rule exists it automatically allows every web robot access to every URL.
-
-.. _database of web robots: http://www.robotstxt.org/wc/active/html/index.html
-
-Bugs, support, questions and headaches
-======================================
-
-Please leave your `questions and problems`_ on the `designated Google Code site`_:
-
-.. _designated Google Code site: http://code.google.com/p/django-robots/
-.. _questions and problems: http://code.google.com/p/django-robots/issues/list
--- robots/models.py
+++ robots/models.py
@@ -1,36 +1,33 @@
 from django.db import models
+from django.core import validators
 from django.contrib.admin.views.doc import simplify_regex
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
+from django.utils.text import get_text_list
 from django.conf import settings
-
-def installed_url_patterns():
-    """
-    Helper function to return URL patterns of the installed applications
-    """
-    paths = []
-    urlconf = __import__(settings.ROOT_URLCONF, {}, {}, [''])
-    for pattern in urlconf.urlpatterns:
-        url_pattern = simplify_regex(pattern.regex.pattern)
-        if not url_pattern.endswith("robots.txt"):
-            paths.append(url_pattern)
-    return " ".join(paths)
+# for backward incompatible changes in 0.97/trunk
+try:
+    from django.db.models import DecimalField as FloatField
+except ImportError:
+    from django.db.models import FloatField 
 
 class Url(models.Model):
     """
-    Defines a URL pattern which should not be allowed to be accessed by a web
-    robot. It's case-sensitive and exact, e.g. "/admin" and "/admin/" are
-    different URLs.
+    Defines a URL pattern for use with a robot exclusion rule. It's 
+    case-sensitive and exact, e.g., "/admin" and "/admin/" are different URLs.
     """
-    pattern = models.CharField(_('pattern'), max_length=255, core=True,
-        help_text=_('This is case-sensitive! Installed apps: %(patterns)s') % {'patterns': installed_url_patterns()})
+    pattern = models.CharField(_('pattern'), max_length=255, core=True, help_text=_("Case-sensitive. A missing trailing slash does also match to files which start with the name of the pattern, e.g., '/admin' matches /admin.html too. Some major search engines allow an asterisk (*) as a wildcard and a dollar sign ($) to match the end of the URL, e.g., '/*.jpg$'."))
+
     class Meta:
         verbose_name = _('url')
         verbose_name_plural = _('url')
+
     class Admin:
         pass
+
     def __unicode__(self):
         return u"%s" % self.pattern
+
     def save(self):
         if not self.pattern.startswith('/'):
             self.pattern = '/' + self.pattern
@@ -38,20 +35,38 @@ class Url(models.Model):
 
 class Rule(models.Model):
     """
-    Defines a abstract rule which should be added to the virtual robots.txt
-    file, disallowing the user agent to access the given URLs. It uses the
-    Site contrib application to enable multiple robots.txt files.
+    Defines an abstract rule which is used to respond to crawling web robots,
+    using the robot exclusion standard, a.k.a. robots.txt. It allows or 
+    disallows the robot identified by its user agent to access the given URLs.
+    The Site contrib app is used to enable multiple robots.txt per instance.
     """
-    user_agent = models.CharField(_('user agent'), max_length=255, 
-        help_text=_("This should be a user agent string like 'Googlebot'. For a full list look at the <a href='http://www.robotstxt.org/wc/active/html/index.html'>database of Web Robots</a>. Enter '*' for matching all user agents."))
-    urls = models.ManyToManyField(Url, help_text="These are URLs which are not allowed to be accessed by web robots.")
+    robot = models.CharField(_('robot'), max_length=255, help_text=_("This should be a user agent string like 'Googlebot'. Enter an asterisk (*) for all user agents. For a full list look at the <a target=_blank href='http://www.robotstxt.org/wc/active/html/index.html'>database of Web Robots</a>."))
+    allowed = models.ManyToManyField(Url, blank=True, validator_list=[validators.RequiredIfOtherFieldNotGiven('disallowed')], related_name="allowed", help_text=_("These are URLs which are allowed to be accessed by web robots."))
+    disallowed = models.ManyToManyField(Url, blank=True, validator_list=[validators.RequiredIfOtherFieldNotGiven('allowed')], related_name="disallowed", help_text=_("These are URLs which are not allowed to be accessed by web robots."))
     sites = models.ManyToManyField(Site)
+    crawl_delay = FloatField(_('crawl delay'), blank=True, null=True, max_digits=3, decimal_places=1, help_text=("From 0.1 to 99.0. This field is supported by some search engines and defines the delay between successive crawler accesses in seconds. If the crawler rate is a problem for your server, you can set the delay up to 5 or 10 or a comfortable value for your server, but it's suggested to start with small values (0.5–1), and increase as needed to an acceptable value for your server. Larger delay values add more delay between successive crawl accesses and decrease the maximum crawl rate to your web server."))
+
     class Meta:
         verbose_name = _('rule')
         verbose_name_plural = _('rules')
-        ordering = ('-user_agent',)
+
     class Admin:
+        fields = (
+            (None, {'fields': ('robot', 'sites')}),
+            (_('URL patterns'), {'fields': ('allowed', 'disallowed')}),
+            (_('Advanced options'), {'classes': 'collapse', 'fields': ('crawl_delay',)}),
+        )
         list_filter = ('sites',)
-        search_fields = ('user_agent',)
+        list_display = ('robot', 'allowed_urls', 'disallowed_urls')
+        search_fields = ('robot','urls')
+
     def __unicode__(self):
-        return u"%s" % self.user_agent
+        return u"%s" % self.robot
+
+    def allowed_urls(self):
+        return get_text_list(list(self.allowed.all()), _('and'))
+    allowed_urls.short_description = _('allowed')
+
+    def disallowed_urls(self):
+        return get_text_list(list(self.disallowed.all()), _('and'))
+    disallowed_urls.short_description = _('disallowed')
