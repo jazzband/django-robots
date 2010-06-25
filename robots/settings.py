d5437521a9915218683e943d5d291d899398e06f
chris@improbable.org <none@none>
2010-06-25 12:20:18 -0400

    Support for multiple sitemaps
    This allows sites which have multiple sitemaps to do something like this in
    settings.py to generate multiple Sitemap lines in robots.txt:
    
    ROBOTS_SITEMAP_URLS = [
        'http://example.org/%s/sitemap.xml' % i for i, j in LANGUAGES
    ]
    
    Internally, the SITEMAP_URL setting changed to a list (SITEMAP_URLS); for
    backwards-compatibility settings.ROBOTS_SITEMAP_URL will automatically be added
    to the list if present.

--- robots/settings.py
+++ robots/settings.py
@@ -1,7 +1,13 @@
 from django.conf import settings
 
+#: A list of one or more sitemaps to inform robots about:
+SITEMAP_URLS = []
+SITEMAP_URLS.extend(getattr(settings,'ROBOTS_SITEMAP_URLS', []))
 
-SITEMAP_URL = getattr(settings,'ROBOTS_SITEMAP_URL', None)
+# For backwards-compatibility, we'll automatically add a single URL
+# to the list:
+if hasattr(settings, 'ROBOTS_SITEMAP_URL'):
+    SITEMAP_URLS.append(settings.ROBOTS_SITEMAP_URL)
 
 USE_SITEMAP = getattr(settings, 'ROBOTS_USE_SITEMAP', True)
 
--- robots/views.py
+++ robots/views.py
@@ -16,25 +16,32 @@ def rules_list(request, template_name='robots/rule_list.html',
     """
     scheme = request.is_secure() and 'https' or 'http'
     current_site = Site.objects.get_current()
-    if settings.SITEMAP_URL:
-        sitemap_url = settings.SITEMAP_URL
-    else:
+
+    sitemap_urls = settings.SITEMAP_URLS
+
+    if not sitemap_urls and settings.USE_SITEMAP:
+        sitemap_url = None
+
         try:
             sitemap_url = reverse('django.contrib.sitemaps.views.index')
         except NoReverseMatch:
             try:
                 sitemap_url = reverse('django.contrib.sitemaps.views.sitemap')
             except NoReverseMatch:
-                sitemap_url = None
-    if sitemap_url is not None and settings.USE_SITEMAP:
-        sitemap_url = "%s://%s%s" % (scheme, current_site.domain, sitemap_url)
+                pass
+
+        if sitemap_url is not None:
+            sitemap_urls.append("%s://%s%s" % (scheme, current_site.domain, sitemap_url))
+
     rules = Rule.objects.filter(sites=current_site)
-    if not rules.count() and sitemap_url is None:
+
+    if not rules.count() and not sitemap_urls:
         status_code = 404
+
     t = loader.get_template(template_name)
     c = RequestContext(request, {
         'rules': rules,
-        'sitemap_url': sitemap_url,
+        'sitemap_urls': sitemap_urls,
     })
     return HttpResponse(t.render(c), status=status_code, mimetype=mimetype)
 
