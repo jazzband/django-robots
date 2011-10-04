af0bb569df46ad0b29273791d374576307288e39
Michael Samoylov <michael.samoylov@gmail.com>
2011-10-04 22:42:00 +0200

    added Host directive

--- robots/settings.py
+++ robots/settings.py
@@ -6,4 +6,6 @@ SITEMAP_URLS.extend(getattr(settings, 'ROBOTS_SITEMAP_URLS', []))
 
 USE_SITEMAP = getattr(settings, 'ROBOTS_USE_SITEMAP', True)
 
+USE_HOST = getattr(settings, 'ROBOTS_USE_HOST', True)
+
 CACHE_TIMEOUT = getattr(settings, 'ROBOTS_CACHE_TIMEOUT', None)
