fec9e071431772cb4ad5b0c8915cefc2df7bb92b
Marcel Cojocaru <marcel.cojocaru@threepillarglobal.com>
2013-06-13 14:15:33 +0300

    LUN-600 -- custom disallowed widget not needed

--- robots/widgets.py
+++ robots/widgets.py
@@ -2,28 +2,18 @@ from django.forms import Select
 from django.utils.safestring import mark_safe
 from django.template import loader, Context
 from django.conf import settings
-#from django.contrib.admin.widgets import FilteredSelectMultiple
 from robots.models import Url
-
-
-# class CustomDisallowedWidget(FilteredSelectMultiple):
-
-#     def render(self, name, value, attrs=None, choices=()):
-#         # value = value or []
-#         # admin, _ = Url.objects.get_or_create(pattern='/admin/')
-#         # if admin.id not in value:
-#         #     value.insert(0, admin.id)
-#         return super(CustomDisallowedWidget, self).render(name, value, attrs, choices)
+from django.core.urlresolvers import reverse
 
 
 class CustomSitesSelector(Select):
 
     def value_from_datadict(self, data, files, name):
+        # the sites filed expects a list of values, while this widget returns
+        # one by default
         return (super(CustomSitesSelector, self).value_from_datadict(data, files, name), )
 
     def render(self, name, value, attrs=None, choices=()):
-        from django.core.urlresolvers import reverse
-        value = value[0] if value else value
         output = super(CustomSitesSelector, self).render(name, value, attrs=attrs, choices=choices)
         t = loader.get_template("robots/reload_disallowed.js")
         c = Context({
