b476b10ba8c21fcfca34b09ec09881a3403f39a0
Алексей Горобчук <ah@en.cx>
2015-05-08 09:14:22 +0300

    Fixed migration

--- robots/migrations/0002_add_free_text.py
+++ robots/migrations/0002_add_free_text.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sites', '0001_initial'),
+        ('0001_initial',),
     ]
 
     operations = [
