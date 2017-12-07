b85ae48b0ba7a95aeba223beb788fe1b4744006b
Tony Narlock <tony@git-pull.com>
2017-12-07 10:31:32 -0600

    fix URL imports for 2.0
    see also: #79, #81

--- tests/test_utils/urls.py
+++ tests/test_utils/urls.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
+import django
+
 import django.contrib.sitemaps.views
 import django.views.i18n
 import django.views.static
 from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap as sitemap_view
 from django.views.decorators.cache import cache_page
+
+if django.VERSION[:2] >= (2, 0):
+    from django.urls import include, re_path as url
+else:
+    from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', django.views.static.serve,  # NOQA
