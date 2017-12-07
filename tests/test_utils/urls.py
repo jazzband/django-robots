d6acbfd888e0110e47e3d33e9ec0053dacbea364
Tony Narlock <tony@git-pull.com>
2017-12-07 10:18:16 -0600

    rm jsi18n url from test_utils example
    django.views.i18n.javascript_catalog was removed in django 2.0.
    This route didn't seem to be used in tests

--- tests/test_utils/urls.py
+++ tests/test_utils/urls.py
@@ -13,7 +13,6 @@ from django.views.decorators.cache import cache_page
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', django.views.static.serve,  # NOQA
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'^jsi18n/(?P<packages>\S+?)/$', django.views.i18n.javascript_catalog),  # NOQA
     url(r'^admin/', include(admin.site.urls)),  # NOQA
     url(r'^/', include('robots.urls')),  # NOQA
     url(r'^sitemap.xml$', sitemap_view, {'sitemaps': []}),
