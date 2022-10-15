11388951c0f17996ef3418e8befb50d4b7ba4687
Tony Narlock <tony@git-pull.com>
2022-10-15 08:50:39 -0500

    refactor!(settings): Simplify settings structure

--- src/robots/settings.py
+++ src/robots/settings.py
@@ -1,23 +1,9 @@
-import sys
+from django.conf import settings
 
-
-class Settings:
-    defaults = {
-        #: A list of one or more sitemaps to inform robots about:
-        "SITEMAP_URLS": ("ROBOTS_SITEMAP_URLS", []),
-        "USE_SITEMAP": ("ROBOTS_USE_SITEMAP", True),
-        "USE_HOST": ("ROBOTS_USE_HOST", True),
-        "CACHE_TIMEOUT": ("ROBOTS_CACHE_TIMEOUT", None),
-        "SITE_BY_REQUEST": ("ROBOTS_SITE_BY_REQUEST", False),
-        "USE_SCHEME_IN_HOST": ("ROBOTS_USE_SCHEME_IN_HOST", False),
-        "SITEMAP_VIEW_NAME": ("ROBOTS_SITEMAP_VIEW_NAME", False),
-    }
-
-    def __getattr__(self, attribute):
-        from django.conf import settings
-
-        if attribute in self.defaults:
-            return getattr(settings, *self.defaults[attribute])
-
-
-sys.modules[__name__] = Settings()
+SITEMAP_URLS = getattr(settings, "ROBOTS_SITEMAP_URLS", [])
+USE_SITEMAP = getattr(settings, "ROBOTS_USE_SITEMAP", True)
+USE_HOST = getattr(settings, "ROBOTS_USE_HOST", True)
+CACHE_TIMEOUT = getattr(settings, "ROBOTS_CACHE_TIMEOUT", None)
+SITE_BY_REQUEST = getattr(settings, "ROBOTS_SITE_BY_REQUEST", False)
+USE_SCHEME_IN_HOST = getattr(settings, "ROBOTS_USE_SCHEME_IN_HOST", False)
+SITEMAP_VIEW_NAME = getattr(settings, "ROBOTS_SITEMAP_VIEW_NAME", False)
