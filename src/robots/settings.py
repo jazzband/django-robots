06131cf04406770b4f7d121a8f4502e8ee1b4017
Tony Narlock <tony@git-pull.com>
2022-10-15 08:50:39 -0500

    refactor!(settings): Simplify settings structure

--- src/robots/settings.py
+++ src/robots/settings.py
@@ -1,9 +1,44 @@
-from django.conf import settings
+import sys
+import typing as t
 
-SITEMAP_URLS = getattr(settings, "ROBOTS_SITEMAP_URLS", [])
-USE_SITEMAP = getattr(settings, "ROBOTS_USE_SITEMAP", True)
-USE_HOST = getattr(settings, "ROBOTS_USE_HOST", True)
-CACHE_TIMEOUT = getattr(settings, "ROBOTS_CACHE_TIMEOUT", None)
-SITE_BY_REQUEST = getattr(settings, "ROBOTS_SITE_BY_REQUEST", False)
-USE_SCHEME_IN_HOST = getattr(settings, "ROBOTS_USE_SCHEME_IN_HOST", False)
-SITEMAP_VIEW_NAME = getattr(settings, "ROBOTS_SITEMAP_VIEW_NAME", False)
+from django.conf import settings
+from django.test.signals import setting_changed
+
+DEFAULT_ROBOTS_SETTINGS: t.Dict[str, t.Any] = {
+    "SITEMAP_URLS": [],
+    "USE_SITEMAP": True,
+    "USE_HOST": True,
+    "CACHE_TIMEOUT": None,
+    "SITE_BY_REQUEST": False,
+    "USE_SCHEME_IN_HOST": False,
+    "SITEMAP_VIEW_NAME": False,
+}
+
+
+def getdefault(setting: str) -> t.Any:
+    return getattr(settings, f"ROBOTS_{setting}", DEFAULT_ROBOTS_SETTINGS[setting])
+
+
+SITEMAP_URLS: t.List[str] = getdefault("SITEMAP_URLS")
+USE_SITEMAP: bool = getdefault("USE_SITEMAP")
+USE_HOST: bool = getdefault("USE_HOST")
+CACHE_TIMEOUT: t.Optional[int] = getdefault("CACHE_TIMEOUT")
+SITE_BY_REQUEST: bool = getdefault("SITE_BY_REQUEST")
+USE_SCHEME_IN_HOST: bool = getdefault("USE_SCHEME_IN_HOST")
+SITEMAP_VIEW_NAME: bool = getdefault("SITEMAP_VIEW_NAME")
+
+
+def reload_robots_settings(
+    setting: str, value: t.Any, enter: bool, **kwargs: object
+) -> None:
+    if not setting.startswith("ROBOTS_"):  # pragma: no cover
+        return
+
+    setting = setting.replace("ROBOTS_", "")
+    if enter:  # setting applied
+        setattr(sys.modules[__name__], setting, value)
+    else:
+        setattr(sys.modules[__name__], setting, getdefault(setting))
+
+
+setting_changed.connect(reload_robots_settings)
