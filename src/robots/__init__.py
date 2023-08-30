5b79816306cb21733ad781146b10dff848bc5cfc
blag <blag@users.noreply.github.com>
2023-08-30 05:36:41 -0700

    Properly format __init__.py for black

--- src/robots/__init__.py
+++ src/robots/__init__.py
@@ -1,8 +1,10 @@
 try:
     from importlib.metadata import version
+
     __version__ = version("django-robots")
 except ImportError:
     from pkg_resources import get_distribution
+
     __version__ = get_distribution("django-robots").version
 
 # needed for Django<3.2
