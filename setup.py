49cf38b6bb2033c17bcbdb86613b3696deec5f87
Tony Narlock <tony@git-pull.com>
2023-09-07 07:05:10 -0500

    packaging: Add trove classifiers for python 3.11, django 4.1 and 4.2

--- setup.py
+++ setup.py
@@ -43,10 +43,13 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Framework :: Django",
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
     ],
 )
