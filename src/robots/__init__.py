febd693c83bfb1697cb8518b13b75c3c07c44bf7
Iwan Trofimtschuk <iwan@djangsters.de>
2023-12-29 13:52:04 +0200

    Remove default_app_config in robots/ __init__.py
    Was only needed for django < 3.2 which are all EOL
    With newer versions of django it's issuing a 
    RemovedInDjango41Warning

--- src/robots/__init__.py
+++ src/robots/__init__.py
@@ -6,6 +6,3 @@ except ImportError:
     from pkg_resources import get_distribution
 
     __version__ = get_distribution("django-robots").version
-
-# needed for Django<3.2
-default_app_config = "robots.apps.RobotsConfig"
